0f2191b147dad8c5bf4085ced68e2f7f54e4b528
44Kwon <kwh3217@gmail.com>
2025-04-07 17:29:37 +0900

    의존성 추가 및 초기세팅

---
+++ src/main/java/practice/usingai/UsingAiApplication.java
@@ -0,0 +1,13 @@
+package practice.usingai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsingAiApplication.class, args);
+    }
+
+}
